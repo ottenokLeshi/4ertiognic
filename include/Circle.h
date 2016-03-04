66cc69b8645aca7a944aca12746e568186cdbaf5
schv <evgeniy.orlov96@gmail.com>
2016-03-05 00:23:02 +0300

    Pushed empty files occasionally

--- include/Circle.h
+++ include/Circle.h
@@ -1,1 +1,34 @@
-#pragma once
+#ifndef _CIRCLE_H
+#define _CIRCLE_H
+#include "Point.h"
+#include "Equation.h"
+
+class Circle : public Point, Equation {
+	Point _center;
+	double _radius;
+	Equation _equation; // x^2 + y^2 + Ax + By + C = 0
+public:
+	Circle() : _center(0, 0), _radius(0), _equation(3) {};
+	Circle(Point center, double radius) : _equation(3) {
+		try {
+			const char* e = "Negative raduis!";
+			if (radius < 0)
+				throw e;
+			_radius = radius;
+			_center = center;
+
+			_equation._a[0] = -_center.getX() / 2;
+			_equation._a[1] = -_center.getY() / 2;
+			_equation._a[2] = (-4 * pow(_radius, 2) + pow(_equation._a[0], 2) + pow(_equation._a[1], 2)) / 4;
+		}
+		catch (const char* e) {
+			std::cout << e;
+		}
+	};
+	Point getCenter() { return _center; };
+	double getRadius() { return _radius; };
+	Equation getEquation() { return _equation; };
+};
+
+
+#endif
--- include/Equation.h
+++ include/Equation.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef _EQUATION_H
+#define _EQUATION_H
+
+class Equation {
+	int _n;
+public:
+	double* _a;
+	Equation() :
+		_n(0), _a(0) {};
+	Equation(const int n) {
+		_n = n;
+		double* _a = (double*)calloc(_n, sizeof(double));
+	};
+};
+
+#endif
