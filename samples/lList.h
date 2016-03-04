316a2231e6b110e89b42c3bcb3749b2863536f69
shabatar <computerstaat@gmail.com>
2016-03-04 21:10:06 +0300

    Fixed delete functions

--- samples/lList.h
+++ samples/lList.h
@@ -5,8 +5,7 @@ struct Cell
 	Cell *next;
 };
 
-template <class T> 
-class List 
+template <class T> class List 
 {
 	Cell<T> *first;
 	Cell<T> *last;
@@ -16,11 +15,22 @@ public:
 		first = NULL;
 		last = NULL;
 	};
-	~List() {};
+	
+	~List() {
+		Cell<T> *start_del = first;
+		Cell<T> *stop_del = last;
+		Cell<T> *curr_del;
+		for (curr_del = start_del; curr_del != stop_del; ) {
+			Cell<T> *temp = curr_del;
+			curr_del = curr_del->next;
+			delete temp;
+		}
+		delete last;
+	};
 
 	void add_item(T d)
 	{
-		Cell <T> *New = new Cell<T>;
+		Cell <T> *New = new Cell <T>;
 		New->data = d;
 		if (first == NULL && last == NULL)
 		{
@@ -55,23 +65,74 @@ public:
 		cout << endl;
 	};
 
-	void del(int start, int stop) //delete in range
+	// delete one element by index
+	void del(int index)
 	{
+		// iteration starts by 1
+		Cell<T> *element = first;
+		// if we need to delete 3rd element from [1, 2, 3, 4, 5, 6]
+		// then element will be 2
+		for (int i = 1; i < index - 1; i++, element = element->next);
+		if (index == 1) 
+		{
+			Cell<T> *temp = first;
+			first = first->next;
+			delete temp;
+		}
+		else 
+		{
+			Cell<T> *temp = element->next;
+			if (element->next != last) // element is not last
+				element->next = element->next->next;
+			else {
+				element->next = NULL;
+				last = element;
+			}
+			delete temp;
+		}
+	}
+
+	// delete in range [start, stop]
+	void del_range(int start, int stop)
+	{
+		if (start > stop) {
+			cout << "start > stop, exit";
+			return;
+		}
 		// iteration starts by 1
 		Cell<T> *start_el = first;
 		Cell<T> *stop_el = first;
+		Cell<T> *temp;
 		int i;
+		
+		// if we need to delete 3..5 elements from [1, 2, 3, 4, 5, 6]
+		// then start_el will be 2, stop_el will be 5
 		for (i = 1; i < start - 1; i++, start_el = start_el->next);
-		for (i = 1; i < stop; i++, stop_el = stop_el->next);
+		for ( ; i < stop; i++, stop_el = stop_el->next);
 
-		if (start_el == first) 
-		{
+		if (start == 1 && stop_el == last) {
+			cout << "Can't delete all list; use destructor for that." << endl;
+			return;
+		}
+
+		// deleting
+		Cell<T> *start_del = (start == 1) ? start_el : start_el->next;
+		Cell<T> *stop_del = stop_el;
+		Cell<T> *curr_del;
+		for (curr_del = start_del; curr_del != stop_del; ) {
+			Cell<T> *temp = curr_del;
+			curr_del = curr_del->next;
+			delete temp;
+		}
+
+		if (start == 1) // deleted first
 			first = stop_el->next;
-		}
-		else if (stop_el == last) // stop = length
-		{ 
+		else if (stop_el == last) { // deleted last
 			last = start_el;
+			last->next = NULL;
 		}
-		else start_el->next = stop_el->next;
-	};
+		else start_el->next = stop_el->next; 
+		delete stop_el;
+	}
+
 };
