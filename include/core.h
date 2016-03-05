5e6d439bb2786d7600ff115d49d0ffb01c437652
77maxikov <77maxikov@mail.ru>
2016-03-05 10:49:43 +0300

    added core

---
+++ include/core.h
@@ -0,0 +1,20 @@
+#ifndef _CORE_H
+#define _CORE_H
+class Core{
+public:
+	Core();
+	void addPoint(double x, double y);
+	void addSegment(double x1, double y1,double x2, double y2);
+	void addCircle(double x, double y,double r);
+
+	
+
+	void addPrimitive(int type, const Array<double> &params){
+		if (type == 1 && params.size() <2) ///ACHTUNG!!!!
+	}
+private:
+
+
+};
+
+#endif
