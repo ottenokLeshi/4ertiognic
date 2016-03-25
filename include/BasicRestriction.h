8f6454bcecc7a1e5a234d31e1fcf46444157c599
Enableee <st048483@student.spbu.ru>
2016-03-25 17:17:40 +0300

    Basic restrictions recovered

---
+++ include/BasicRestriction.h
@@ -0,0 +1,15 @@
+#ifndef _BASIC_RESTRICTION_H
+#define _BASIC_RESTRICTION_H
+
+#include "restrict_types.h"
+#include "objects.h"
+
+class BasicRestriction
+ {
+	public:
+		BasicRestriction() {}
+		int get_id()const {}
+		virtual RestrictType get_type()const = 0;
+		virtual double violation()const = 0;
+};
+#endif
