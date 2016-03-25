1488f28372fc22a8fb82398908cc1c2e3bc90b3c
AlexPshe <psheborov@gmail.com>
2016-03-25 17:12:53 +0300

    fixed

--- include/RestrP2PDIST.h
+++ include/RestrP2PDIST.h
@@ -2,7 +2,7 @@
 #define _RESTR_P2PDIST_H
 #include "objects.h"
 #include "BasicRestriction.h"
-
+#include "math.h"
 class RestrP2PDIST:BasicRestriction
 {
 public:
@@ -13,19 +13,14 @@ public:
 		_dist = dist;
 	};
 	virtual ~RestrP2PDIST() {}
-	virtual RestrictType get_type()const { return RT_P2PDIST; };
-        virtual double violation() const {
-        	double realDist = Point::dist_points(&_A, &_B);
-        	return *_dist - realDist;
+	virtual RestrictType get_type() { return RT_P2PDIST; };
+        virtual double violation() {
+        	return *_dist - sqrt((A->getX() - B->getX())*(A->getX() - B->getX()) + (A->getY() - B->getY())*(A->getY() - B->getY()));
         }
 private:
 	Point *_A;
 	Point *_B;
-	double _dist;
+	double *_dist;
 };
-/*
- * In objects.h
- * static dist_points(Point *A, Point *B);
- *
-*/
+
 #endif
