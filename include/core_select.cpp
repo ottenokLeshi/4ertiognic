fe84f1cd3100504062e43c154eaa32a2001a13d0
AlexPshe <psheborov@gmail.com>
2016-03-30 17:51:25 +0300

    added selectByPoint

--- include/core_select.cpp
+++ include/core_select.cpp
@@ -9,30 +9,30 @@ List<unsigned>* Core::selectByRect(double x1, double y1, double x2, double y2) {
 	for (List<GraphPrimitive*>::Marker mar(objects);mar.exist();mar.move_next()) {
 		if (mar.get_current()->isInRect(x1, y1, x2, y2)) {
 			PickedObjects->addElem(p, mar.get_current()->showId());
+			mar.get_current()->changePick(1);
 		}
+		else mar.get_current()->changePick(0); // delete last selection
 	}
 		return PickedObjects;
 }
-/*
-GraphPrimitive* Core::selectByPoint(Point p) {
-	GraphPrimitive* PickedObject = NULL;
+
+unsigned Core::selectByPoint(Point p) {
 	const double EPS = 1e-10;
-	double current_distance, min_distance = 0;
-	bool isEmpty = true;
-	for (int i = 0; i < objects.size(); i++) {
-		current_distance = objects.get_elem(i)->distanceToPoint(p.getX(),p.getY());
-		if (current_distance <= EPS) {
-			if (isEmpty) {
-				PickedObject = objects.get_elem(i);
-				isEmpty = false;
-			}
-			else if (current_distance < min_distance) {
-				PickedObject = objects.get_elem(i);
-				min_distance = current_distance;
-			}
+	double current_distance, min_distance = EPS;
+	unsigned min_id = 0;
+	for (List<GraphPrimitive*>::Marker mar(objects);mar.exist();mar.move_next()) {
+		current_distance = mar.get_current()->distanceToPoint(p.getX(),p.getY());
+		if (current_distance < min_distance) {
+			min_distance = current_distance;
+			min_id = mar.get_current()->showId();
 		}
-	}
-	return PickedObject;
+}	
+	return min_id;
 }
-*/
+
 #endif
+
+int main() {
+
+	return 0;
+}
