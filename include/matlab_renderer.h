5616bad923cb39e1beafee4251e7d5a8168f859c
AlexPshe <psheborov@gmail.com>
2016-04-08 00:28:34 +0300

    update MatlabRenderer, added MatlabRenderer test

--- include/matlab_renderer.h
+++ include/matlab_renderer.h
@@ -3,14 +3,20 @@
 #include <iostream>
 #include <fstream> 
 #include <string>
-#include "../include/core.h"
+
 
 using namespace std;
 class MatlabRenderer {
+	string _filename;
 public:
-	bool drawPrimitive(string filename,Primitive_Type type, Array<double> parametrs, size_t markersize) {
+	MatlabRenderer(string filename)
+		: _filename(filename) {}
+
+	~MatlabRenderer() {}
+
+	bool drawPrimitive(Primitive_Type type, Array<double> parametrs, size_t markersize) {
 		fstream f;
-		f.open(filename);
+		f.open(_filename);
 		if (!f.is_open()) return false;
 		long file_size;
 		f.seekg(0, ios::end);
@@ -18,9 +24,9 @@ public:
 		if (file_size == 0) {
 			string namefunc = "";
 			size_t t = 0;
-			for (t = filename.size();t != 0;t--)
-				if (filename[t - 1] == '/') break;
-			for (size_t i = t+1;i <= filename.size() - 2;i++) namefunc = namefunc + filename[i - 1];
+			for (t = _filename.size();t != 0;t--)
+				if (_filename[t - 1] == '/') break;
+			for (size_t i = t + 1;i <= _filename.size() - 2;i++) namefunc = namefunc + _filename[i - 1];
 			f << "function res = " << namefunc << "()" << endl;
 			f << "res = 0" << endl;
 			f << "figure; hold on" << endl;
@@ -31,8 +37,8 @@ public:
 		case IsPoint:
 			if (parametrs.size() == 2) {
 				double x = parametrs[0], y = parametrs[1];
-				f << "plot([" << x << " "<< x << "],[" <<y<< 
-					" " << y<< "], 'LineWidth', 2, 'Marker', '.', 'MarkerSize', " << markersize << ")" << endl;
+				f << "plot([" << x << " " << x << "],[" << y <<
+					" " << y << "], 'LineWidth', 2, 'Marker', '.', 'MarkerSize', " << markersize << ")" << endl;
 			}
 			break;
 
@@ -48,7 +54,7 @@ public:
 			if (parametrs.size() == 3) {
 				double x = parametrs[0], y = parametrs[1], rad = parametrs[2];
 				f << "plot(" << x << " + " << rad << " * cos(0:0.001 : 2 * pi), " << y << " + " << rad <<
-					" * sin(0:0.001 : 2 * pi), 'LineWidth', 2, 'Marker', '.', 'MarkerSize', "<< markersize <<")" << endl;
+					" * sin(0:0.001 : 2 * pi), 'LineWidth', 2, 'Marker', '.', 'MarkerSize', " << markersize << ")" << endl;
 			}
 			break;
 		}
