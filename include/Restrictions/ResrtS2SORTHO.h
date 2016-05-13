95c50b08fee5fb3b46d076e30a8f8996e777cb8b
schv <evgeniy.orlov96@gmail.com>
2016-05-13 21:30:01 +0300

    diifs

--- include/Restrictions/ResrtS2SORTHO.h
+++ include/Restrictions/ResrtS2SORTHO.h
@@ -14,7 +14,44 @@ public:
 	double violation()const {
 		return  (1.5707963267949 - _S1->Angle(_S2)); //1.5707963267949 - 90 in radian
 	}
-	virtual double diff(size_t par) { return 0; }
+	virtual double diff(size_t par) {
+		double diff(size_t par) {
+			double _p1x, _p2x, _p3x, _p4x, _p1y, _p2y, _p3y, _p4y;
+			_p1x = _S1->getP1()->getX(), _p2x = _S1->getP2()->getX(), _p3x = _S2->getP1()->getX(), _p4x = _S2->getP2()->getX(),
+				_p1y = _S1->getP1()->getY(), _p2y = _S1->getP2()->getY(), _p3y = _S2->getP1()->getY(), _p4y = _S2->getP2()->getY();
+
+			double skalyar = (_p1x - _p2x)*(_p3x - _p4x) + (_p1y - _p2y)*(_p3y - _p4y);
+			double arccos = (-1) / sqrt(1 - pow(_S1->cosAngle(_S2), 2));
+			switch (par) {
+			case 1: // parametr _p1x
+				return (skalyar*(_p1x - _p2x) / (_S2->getLength()*pow(_S1->getLength(), 3)) - (_p3x - _p4x) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 2: // parametr _p2x
+				return (-skalyar*(_p1x - _p2x) / (_S2->getLength()*pow(_S1->getLength(), 3)) + (_p3x - _p4x) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 3: // parametr _p3x
+				return (skalyar*(_p3x - _p4x) / (_S1->getLength()*pow(_S2->getLength(), 3)) - (_p1x - _p2x) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 4: // parametr _p4x
+				return (-skalyar*(_p3x - _p4x) / (_S1->getLength()*pow(_S2->getLength(), 3)) + (_p1x - _p2x) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 5: // parametr _p1y
+				return (skalyar*(_p1y - _p2y) / (_S2->getLength()*pow(_S1->getLength(), 3)) - (_p3y - _p4y) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 6: // parametr _p2y
+				return (-skalyar*(_p1y - _p2y) / (_S2->getLength()*pow(_S1->getLength(), 3)) + (_p3y - _p4y) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 7: // parametr _p3y
+				return (skalyar*(_p3y - _p4y) / (_S1->getLength()*pow(_S2->getLength(), 3)) - (_p1y - _p2y) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 8: // parametr _p4y
+				return (-skalyar*(_p3y - _p4y) / (_S1->getLength()*pow(_S2->getLength(), 3)) + (_p1y - _p2y) / (_S1->getLength()*_S2->getLength()))*arccos;
+				break;
+			case 9: // _angle
+				return 1;
+				break;
+			}
+		}
 
 	double getAngl() const {
 		return  _S1->Angle(_S2);
--- include/Restrictions/RestrP2SDISTEX.h
+++ include/Restrictions/RestrP2SDISTEX.h
@@ -28,7 +28,238 @@ public:
 				return *_min - sqrt(vec2x*vec2x + vec2y*vec2y);
 		}
 	}
-	virtual double diff(size_t par) { return 0; }
+	virtual double diff(size_t par) { 
+		virtual double diff(size_t par) {
+			double x1 = _beg->getX(), x2 = _end->getX(), y1 = _beg->getY(), y2 = _end->getY(), xt = _t->getX(), yt = _t->getY();
+			if (((x1 - xt)*(x1 - x2) + (y1 - yt)*(x2 - y2))*((x2 - xt)*(x1 - x2) + (y2 - yt)*(x2 - y2)) < 0) {
+				if (x1 > x2 && y1 != y2) {
+					switch (par) {
+					case 1:
+						return 2 * (yt - y2)*pow((1 + ((y1 - y2)*(y1 - y2)) / ((x1 - x2)*(x1 - x2))), -3 / 2)*
+							((y1 - y2)*(y1 - y2))*pow((x1 - x2), -3) +
+							2 * ((y1 - y2)*(y1 - y2))*(xt - x2)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2);
+						break;
+					case 2:
+						return -2 * (xt - x2)*pow((1 + ((x1 - x2)*(x1 - x2)) / ((y1 - y2)*(y1 - y2))), -3 / 2)*
+							((x1 - x2)*(x1 - x2))*pow((y1 - y2), -3) -
+							2 * ((x1 - x2)*(x1 - x2))*(yt - y2)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2);
+						break;
+					case 3:
+						return 2 * (yt - y2)*pow((1 + ((y1 - y2)*(y1 - y2)) / ((x1 - x2)*(x1 - x2))), -3 / 2)*
+							((y1 - y2)*(y1 - y2))*pow((x2 - x1), -3) -
+							(y1 - y2)*(-(xt - x2)*(x2 - x2)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2) +
+								sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2))));
+						break;
+					case 4:
+						return -2 * (xt - x2)*pow((1 + ((y1 - y2)*(y1 - y2)) / ((x1 - x2)*(x1 - x2))), -3 / 2)*
+							((x1 - x2)*(x1 - x2))*pow((y2 - y1), -3) -
+							(x1 - x2)*((yt - y2)*(y2 - y1)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2) -
+								sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2))));
+						break;
+					case 5:
+						return (y1 - y2) / sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)));
+						break;
+					case 6:
+						return -(x1 - x2) / sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)));
+						break;
+					case 7:
+						return 0;
+						break;
+					}
+				}
+				else if (x1 < x2 && y1 != y2) {
+					switch (par) {
+					case 1:
+						return -(2 * (yt - y2)*pow((1 + ((y1 - y2)*(y1 - y2)) / ((x1 - x2)*(x1 - x2))), -3 / 2)*
+							((y1 - y2)*(y1 - y2))*pow((x1 - x2), -3) +
+							2 * ((y1 - y2)*(y1 - y2))*(xt - x2)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2));
+						break;
+					case 2:
+						return -(-2 * (xt - x2)*pow((1 + ((x1 - x2)*(x1 - x2)) / ((y1 - y2)*(y1 - y2))), -3 / 2)*
+							((x1 - x2)*(x1 - x2))*pow((y1 - y2), -3) -
+							2 * ((x1 - x2)*(x1 - x2))*(yt - y2)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2));
+						break;
+					case 3:
+						return -(2 * (yt - y2)*pow((1 + ((y1 - y2)*(y1 - y2)) / ((x1 - x2)*(x1 - x2))), -3 / 2)*
+							((y1 - y2)*(y1 - y2))*pow((x2 - x1), -3) -
+							(y1 - y2)*(-(xt - x2)*(x2 - x2)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2) +
+								sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)))));
+						break;
+					case 4:
+						return -(-2 * (xt - x2)*pow((1 + ((y1 - y2)*(y1 - y2)) / ((x1 - x2)*(x1 - x2))), -3 / 2)*
+							((x1 - x2)*(x1 - x2))*pow((y2 - y1), -3) -
+							(x1 - x2)*((yt - y2)*(y2 - y1)*pow(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)), -3 / 2) -
+								sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2)))));
+						break;
+					case 5:
+						return -((y1 - y2) / sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2))));
+						break;
+					case 6:
+						return -(-(x1 - x2) / sqrt(((y1 - y2)*(y1 - y2)) + ((x1 - x2)*(x1 - x2))));
+						break;
+					case 7:
+						return 0;
+						break;
+					}
+				}
+				else if (x1 == x2 && y1 != y2) {
+					if (y1 > y2) {
+						switch (par) {
+						case 1:
+							return 0;
+							break;
+						case 2:
+							return 0;
+							break;
+						case 3:
+							return -1;
+							break;
+						case 4:
+							return 0;
+							break;
+						case 5:
+							return 1;
+							break;
+						case 6:
+							return 0;
+							break;
+						case 7:
+							return 0;
+							break;
+						}
+					}
+					if (y1 < y2) {
+						switch (par) {
+						case 1:
+							return 0;
+							break;
+						case 2:
+							return 0;
+							break;
+						case 3:
+							return 1;
+							break;
+						case 4:
+							return 0;
+							break;
+						case 5:
+							return -1;
+							break;
+						case 6:
+							return 0;
+							break;
+						case 7:
+							return 0;
+							break;
+						}
+					}
+				}
+				else if (y1 == y2 && x1 != x2) {
+					if (x1 > x2) {
+						switch (par) {
+						case 1:
+							return 0;
+							break;
+						case 2:
+							return 0;
+							break;
+						case 3:
+							return 0;
+							break;
+						case 4:
+							return 1;
+							break;
+						case 5:
+							return 0;
+							break;
+						case 6:
+							return -1;
+							break;
+						case 7:
+							return 0;
+							break;
+						}
+					}
+					if (x1 < x2) {
+						switch (par) {
+						case 1:
+							return 0;
+							break;
+						case 2:
+							return 0;
+							break;
+						case 3:
+							return 0;
+							break;
+						case 4:
+							return -1;
+							break;
+						case 5:
+							return 0;
+							break;
+						case 6:
+							return 1;
+							break;
+						case 7:
+							return 0;
+							break;
+						}
+					}
+				}
+				else return 0;
+			}
+			else if (sqrt((x1 - xt)*(x1 - xt) + (y1 - yt)*(y1 - yt)) < sqrt((x2 - xt)*(x2 - xt) + (y2 - yt)*(y2 - yt))) {
+				switch (par) {
+				case 1:
+					return (x1 - xt) / sqrt((x1 - xt)*(x1 - xt) + (y1 - yt)*(y1 - yt));
+					break;
+				case 2:
+					return (y1 - yt) / sqrt((x1 - xt)*(x1 - xt) + (y1 - yt)*(y1 - yt));
+					break;
+				case 3:
+					return 0;
+					break;
+				case 4:
+					return 0;
+					break;
+				case 5:
+					return (xt - x1) / sqrt((x1 - xt)*(x1 - xt) + (y1 - yt)*(y1 - yt));
+					break;
+				case 6:
+					return (yt - y1) / sqrt((x1 - xt)*(x1 - xt) + (y1 - yt)*(y1 - yt));
+					break;
+				case 7:
+					return 1;
+					break;
+				}
+			}
+			else {
+				switch (par) {
+				case 1:
+					return 0;
+					break;
+				case 2:
+					return 0;
+					break;
+				case 3:
+					return (x2 - xt) / sqrt((x2 - xt)*(x2 - xt) + (y2 - yt)*(y2 - yt));
+					break;
+				case 4:
+					return (y2 - yt) / sqrt((x2 - xt)*(x2 - xt) + (y2 - yt)*(y2 - yt));
+					break;
+				case 5:
+					return (xt - x2) / sqrt((x2 - xt)*(x2 - xt) + (y2 - yt)*(y2 - yt));
+					break;
+				case 6:
+					return (yt - y2) / sqrt((x2 - xt)*(x2 - xt) + (y2 - yt)*(y2 - yt));
+					break;
+				case 7:
+					return 1;
+					break;
+				}
+			}
+		}
+	}
 
 	double getDist() const {
 			double x1 = _beg->getX(), x2 = _end->getX(), y1 = _beg->getY(), y2 = _end->getY(), xt = _t->getX(), yt = _t->getY();
